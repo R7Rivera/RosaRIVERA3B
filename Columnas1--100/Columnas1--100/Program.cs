6f3b55a106dbf52267525de481976e31927630c4
R7Rivera <56278637+R7Rivera@users.noreply.github.com>
2019-10-14 02:46:50 -0500

    Add files via upload

---
+++ Columnas1--100/Columnas1--100/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Columnas1__100
+{
+    class Program
+    {
+    
+        static void Main(string[] args)
+        {
+            int inicial, inicial1;
+            Console.WriteLine("**********COLUMNA DE NUMEROS DEL 1 AL 100******** | ********COLUMNA DE NUMEROS DEL 100 AL 1 ***********");
+
+            Console.WriteLine("\t \t°°°°°°°°°°°°°°°°°°°°°°°°°°° \n \n");
+
+            {
+
+                {
+
+
+                    for (inicial = 1; inicial <= 100; inicial++)
+                    {
+                        Console.WriteLine(" " + inicial);
+
+                        inicial1 = 101 - inicial;
+                        Console.WriteLine("   " + "       " + "      " + "     " + "  " + "  " + (inicial1) + "   " + "   ");
+
+                    }
+                    Console.ReadKey();
+                }
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ TablaMultiplicar15/TablaMultiplicar15/Program.cs
@@ -0,0 +1,28 @@
+﻿//Crear el programa tablaMultiplicar que escriba la tabla de multiplicar del número 1 al número 15
+using System;
+
+namespace TablaMultiplicar15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                int numero, Inicial;
+                int resultado;
+                do
+                {
+                Console.Write("*********TABLA DE MULTIPLICAR***********");
+                Console.Write(" \n\n ");
+                Console.Write("DIGITE NÚMERO DEL 1 AL 15:\n");
+                numero = int.Parse(Console.ReadLine());
+                for (Inicial = 1; Inicial <= 15; Inicial++)
+                {
+                    resultado = numero * Inicial;
+                    Console.WriteLine("\n{0} * {1} = {2}", numero, Inicial,resultado);
+                }
+                Console.Write("PULSE UNA TECLA SI DESEA CONTINUAR  :\n"); 
+                Console.ReadKey();
+            } while (Inicial != 0);
+        }
+    }
+}
